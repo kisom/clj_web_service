6c8200237d776be74554dac1836d827db5ddd49e
Kyle Isom <coder@kyleisom.net>
2012-01-26 20:43:32 +0300

    use common.clj for common functions

--- src/web_service/views/common.clj
+++ src/web_service/views/common.clj
@@ -1,24 +1,23 @@
 (ns web_service.views.common
   (:use [noir.core :only [defpartial defpage]]
-        [cheshire.core]
-        [using_set_theory.sample_library])
-  (:require [clojure.string]))
+        [cheshire.core])
+  (:require [clojure.string]
+            [noir.response]))
 
 (def version "1.0.0")
+
+(defn text-out
+  "Output a string as text/plain."
+  [body & args]
+  (noir.response/content-type "text/plain" body))
+
+(defn json-out
+  "Output a string as json plaintext."
+  [body & args]
+  (noir.response/json body))
+;  (text-out (cheshire.core/generate-string body)))
 
 (defn multiline
   "Easy way to put together a bunch of strings."
   [& args]
   (if (nil? args) "" (clojure.string/join " " args)))
-  
-(defpage "/" []
-  (multiline
-   "This is an illustrative web API for the book library. I wrote this for "
-   "a blog post on using set theory:"
-   "http://kisom.github.com/blog/2012/01/??/using-set-theory/"))
-
-(defpage "/api" []
-  (generate-string { :version version }))
-
-(defpage "/help" []
-  (slurp "resources/public/help"))
