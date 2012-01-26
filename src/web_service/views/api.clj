90d4265154f7132badd6f8cae33599274c44f48b
Kyle Isom <coder@kyleisom.net>
2012-01-26 20:44:17 +0300

    add title functionality

--- src/web_service/views/api.clj
+++ src/web_service/views/api.clj
@@ -1,5 +1,6 @@
 (ns web_service.views.api
   (:use [noir.core :only [defpartial defpage]]
+        [web_service.views.common]
         [using_set_theory.library :only [book-str in? ]]
         [using_set_theory.sample_library])
   (:require [clojure.string]
@@ -16,10 +17,17 @@
 
 (defpage "/api/books" []
   (let [books (get-library)]
-    (json/generate-string books)))
+    (json-out books)))
 
 
 (defpage "/api/books/format/:fmt" {:keys [fmt]}
-  (json/generate-string
+  (json-out
    (filter-format fmt (get-library))))
 
+(defpage "/api/books/titles" []
+  (json-out
+   (map :title (get-library))))
+
+(defpage "/api/books/title/:title" {:keys [title]}
+  (json-out
+   (filter #(= title (:title %)) (get-library))))
